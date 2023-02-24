2e28e3a632dc0f23155d5fcdd51b6ef516ca9e9d
Haneen-Abdulgllil <haneenabdulglil2018@yahoo.com>
2023-02-24 11:20:05 +0300

    assignment complated

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
   // Write your code here!
+
+  if(value <= 400){
+    return "This one is on me!";
+  }
+  else if(value > 400 & value < 2000){
+    return "That will be twenty bucks.";
+  }
+  else if(value > 2000 & value< 2500){
+    return "I will gladly take your thirty bucks.";
+  }
+  else if(value > 2500){
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(isCity){
+  const city = 'NYC';
+  return (isCity === city ? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous' :
+      return 'Thank you.'
+    case 'thanks for everything' :
+      return 'Bye.'
+    default:
+  }
 }
